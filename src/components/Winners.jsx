6cd50c45824d23806fe297a419e26e934c410834
EslamEssamM <ee3@pme.suezuni.edu.eg>
2024-06-17 04:29:26 +0300

    animation added

--- src/components/Winners.jsx
+++ src/components/Winners.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import  Lottie  from "lottie-react";
+import fireworksAnimation from "../assets/animations.json"; // Update this path
 
-const WinnerCard = ({ data,no, reveal, setReveal, winners }) => {
+const WinnerCard = ({ data, no, reveal, setReveal, winners }) => {
   const [current, setCurrent] = useState(null);
   const [pulse, setPulse] = useState(false);
+  const [showFireworks, setShowFireworks] = useState(false);
 
   useEffect(() => {
     let interval;
@@ -12,7 +15,7 @@ const WinnerCard = ({ data,no, reveal, setReveal, winners }) => {
       interval = setInterval(() => {
         const randomUser = data[Math.floor(Math.random() * data.length)];
         setCurrent(randomUser);
-      }, 100); // Change users every 100ms
+      }, 100);
     }
 
     return () => clearInterval(interval);
@@ -21,40 +24,28 @@ const WinnerCard = ({ data,no, reveal, setReveal, winners }) => {
   useEffect(() => {
     if (reveal) {
       setTimeout(() => {
-        setReveal(false); // Stop revealing after 5 seconds
+        setReveal(false);
         setCurrent(winners[no]);
-
+        setShowFireworks(true);
+        setTimeout(() => setShowFireworks(false), 3000); // Hide fireworks after 3 seconds
       }, 4000);
     }
-  }, [reveal, setReveal]);
+  }, [reveal, setReveal, no, winners]);
 
-  useEffect(() => {
-    if (!reveal && current) {
-      const pulseInterval = setInterval(() => {
-        setPulse((prevPulse) => !prevPulse);
-      }, 200000); // Pulse every 500ms
-
-      return () => clearInterval(pulseInterval);
-    }
-  }, [reveal, current]);
-
-  const variants = {
-    hidden: { opacity: 0, y: 50 },
-    show: { opacity: 1, y: 0 },
-  };
+ 
 
   return (
     <AnimatePresence>
       {current && (
         <motion.div
           initial={{ scale: 0, rotate: 180 }}
-          animate={{ scale: pulse ? 1.1 : 1, rotate: 0 }}
+          animate={{ scale:  1, rotate: 0 }}
           exit={{ scale: 0, rotate: 180 }}
           transition={{ type: "spring", stiffness: 260, damping: 20 }}
-          className=" w-1/3 max-w-96  mx-4 my-0 mt-0  bg-[#f0ebe5] p-4 rounded shadow"
+          className="w-1/3 relative max-w-96 mx-4 my-0 mt-0 bg-[#f0ebe5] p-4 rounded shadow"
         >
           <div className="backdrop:bg-white shadow-lg rounded-lg p-2 border border-gray-200">
-            <p className="text-lg font-bold  text-center text-green-700">
+            <p className="text-lg font-bold text-center text-green-700">
               {`الفائز رقم ${no + 1}`}
             </p>
             <p className="text-lg font-bold text-secondary text-center text-nowrap">
@@ -83,8 +74,21 @@ const WinnerCard = ({ data,no, reveal, setReveal, winners }) => {
                   <td className="border px-4 py-2 text-primary">جوال</td>
                 </tr>
               </tbody>
-            </table>{" "}
+            </table>
           </div>
+          {showFireworks && (
+            <Lottie
+              animationData={fireworksAnimation}
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                top: "-100px",
+                background: "transparent",
+              }}
+            />
+            
+          )}
         </motion.div>
       )}
     </AnimatePresence>
@@ -127,6 +131,7 @@ const Winners = ({ data }) => {
       <h2 className="text-4xl font-bold mb-4 text-center text-primary">
         الفائزون في المسابقة الثقافية لحج عام 1445هـ
       </h2>
+      
       <div className="w-full flex flex-row-reverse  justify-evenly   ">
         {winners.map((winner, index) => (
           <WinnerCard
